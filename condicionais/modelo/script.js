e8d16e275b888b8dfd2f0d28b9d61f0a810353c6
Natanaeln03 <natancnascimento88@gmail.com>
2024-05-29 10:08:44 -0300

    modificado

--- condicionais/modelo/script.js
+++ condicionais/modelo/script.js
@@ -1,26 +1,25 @@
-function carregar (){
+function carregar () {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas`
-
-    if (hora >= 0 && hora < 12){
-        //Bom Dia
+    
+    if (hora >=0 && hora < 12){
+        //Bom Dia!
         img.src = 'foto-manha.png'
         document.body.style.background = '#fbe271'
-        document.getElementById('texto')[0].style.color = 'black'
-        document.getElementById('texto')[1].style.color = 'black'
-    } else if (hora >= 12 && hora <= 18){
-        //Boa Tarde
+        document.getElementsByClassName('texto')[0].style.color = 'black'
+        document.getElementsByClassName('texto')[1].style.color = 'black'
+    } else if (hora >= 12 && hora <=18){
+        //Boa Tarde!
         img.src = 'foto-tarde.png'
         document.body.style.background = '#fe8920'
-
     } else {
         //Boa Noite
         img.src = 'foto-noite.png'
         document.body.style.background = '#1f2540'
-
     }
+
 }
